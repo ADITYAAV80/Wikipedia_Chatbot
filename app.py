70f5934b7133c27c699df7e658650c50edece5aa
ADITYAAV80 <adityaav80@gmail.com>
2025-03-24 13:48:33 +0530

    chatbot

---
+++ app.py
@@ -0,0 +1,158 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_groq import ChatGroq
+import bs4
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.chains import create_history_aware_retriever
+from langchain.prompts import MessagesPlaceholder
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+import validators
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_APIKEY")
+os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+st.set_page_config(page_title="Wikipedia Article summarizer",page_icon="📖")
+st.title("Wikipedia Article Summarizer")
+
+
+if "store" not in st.session_state:
+    st.session_state.store = {"messages":[{"role":"assistant", "content":"Hi steps to follow are first upload your wiki link using sidebar and then press load the document. Once it's loaded you can start asking your queries here"}], "vector_db": None, "retriever": None}
+
+with st.sidebar:
+
+    session_id = st.text_input("Enter your session ID",value="default_session")
+
+    wikilink = st.text_input("Enter your wikipedia link")
+
+    if wikilink!="" and not validators.url(wikilink):
+        st.error("Invalid URL. Please enter a valid Wikipedia link.")
+
+    elif wikilink!="" and "wikipedia.org" not in wikilink:
+        st.error("Please enter a Wikipedia URL.")
+    
+    elif wikilink=="":
+        st.warning("Enter your link to get started")
+
+    elif st.button("Load the document"):
+        loader = WebBaseLoader(
+            web_path=wikilink,
+            bs_kwargs= dict(parse_only=bs4.SoupStrainer(
+                        class_ = ("mw-page-title-main","mw-body-content"))))
+        
+        document = loader.load()
+        rcs = RecursiveCharacterTextSplitter(chunk_size=3000,chunk_overlap=100)
+        split_docs = rcs.split_documents(document)
+        
+        embeddings = OpenAIEmbeddings(
+            #model_name="sentence-transformers/all-MiniLM-L6-v2",
+        )
+
+        st.session_state.store["vector_db"] = FAISS.from_documents(split_docs, embeddings)
+        st.session_state.store["retriever"] = st.session_state.store["vector_db"].as_retriever()
+        st.session_state.store["messages"].append({"role": "assistant", "content": "Article is loaded!"})
+
+
+for msg in st.session_state.store["messages"]:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+
+if st.session_state.store["retriever"] is not None:
+    # Prepare prompts and chain
+    llm = ChatGroq(model="Gemma2-9b-It")
+
+    sysem_prompt = """
+    You are a Wikipedia Article Summarizer. Your task is to extract key points from Wikipedia articles and present them in a clear, concise, and factual manner. 
+    Keep responses brief and to the point while maintaining essential details.
+    Ensure the summary is neutral and objective, without personal opinions.
+    Use simple and accessible language suitable for a broad audience.
+    If asked for more details, provide a structured breakdown (e.g., bullet points or short paragraphs).
+
+    ❗ **Strict Rule:** You **MUST** answer **ONLY** based on the provided Wikipedia article.
+    If the query is unrelated to the loaded Wikipedia content, respond with:
+    "I can only answer questions based on the loaded Wikipedia article. Please provide a relevant query."
+
+    If **no article is loaded**, respond with:
+    "Please upload a Wikipedia article first using the sidebar and load it before asking questions."
+
+    <context>
+    {context}
+    </context>
+    """
+
+
+
+    final_prompt = ChatPromptTemplate.from_messages([
+        ("system",sysem_prompt),
+        MessagesPlaceholder(variable_name="stored_chat_history"),
+        ("human","{input}")
+    ])
+
+    #fills context
+    document_chain = create_stuff_documents_chain(llm,final_prompt)
+
+    system_summarizer_prompt = """
+    Given a chat history and the latest user question
+    which might reference context in the chat history,
+    formulate a standalone question which can be understood
+    without the chat history. Do NOT answer the question, 
+    just reformulate it if needed and otherwise return it as is.
+    """
+
+    summarizer_prompt = ChatPromptTemplate.from_messages([
+        ("system",system_summarizer_prompt),
+        MessagesPlaceholder(variable_name="stored_chat_history"),
+        ("human","{input}")
+    ])
+
+    history_aware_retriever=create_history_aware_retriever(llm,st.session_state.store["retriever"],summarizer_prompt)
+    #fills input
+    rag_chain = create_retrieval_chain(history_aware_retriever,document_chain)
+
+    def get_session_history(session:str)->BaseChatMessageHistory:
+            if session_id not in st.session_state.store:
+                st.session_state.store[session_id]=ChatMessageHistory()
+            return st.session_state.store[session_id]
+        
+    conversational_rag_chain=RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="stored_chat_history",
+        output_messages_key="answer"
+    )
+
+
+user_query = st.chat_input(placeholder="Enter your query")
+
+
+
+if user_query:
+    
+    st.session_state.store["messages"].append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+    
+    similar_docs = st.session_state.store["vector_db"].similarity_search(user_query)
+
+    response = conversational_rag_chain.invoke(
+         {"input":user_query},
+         config={"configurable": {"session_id":session_id}},
+         )
+    
+    st.session_state.store["messages"].append({"role": "assistant", "content": response["answer"]})
+    st.chat_message("ai").write(response["answer"])
